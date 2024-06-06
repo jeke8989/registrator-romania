f12a3698e66a2f060ddc7b08d4a7769bb1fee242
devtolmachev <dev.tolmachev@gmail.com>
2024-06-06 20:38:55 +0300

    add loguru handler

--- registrator_romania/main.py
+++ registrator_romania/main.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta
 
 from zoneinfo import ZoneInfo
 
+from loguru import logger
+
+from registrator_romania.bot import send_msg_into_chat
 from registrator_romania.scheduler import start_scheduler
 
 
@@ -15,8 +18,9 @@ async def keep_running() -> None:
 async def main() -> None:
     """Entrypoint."""
     dt = datetime.now() + timedelta(seconds=10)
+    logger.add(send_msg_into_chat, level="INFO")
     await start_scheduler(
-        hour=8, minute=59, seconds=30, timezone=ZoneInfo("Europe/Moscow")
+        hour=8, minute=59, second=30, timezone=ZoneInfo("Europe/Moscow")
     )
     await keep_running()
 
